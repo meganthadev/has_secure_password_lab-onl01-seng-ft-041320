43bcd99e77ffaaae11ca635eb349d319a222013f
Megan Horsfield <meganthadev@gmail.com>
2020-07-07 04:00:01 +0000

    Automatically backed up by Learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+
+  def new 
+  end 
+  
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user == nil
+      redirect_to 'new'
+    else
+      return head(:forbidden) unless @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to homepage_path(@user)
+    end
+  end
+  
+  def destroy
+  end 
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
