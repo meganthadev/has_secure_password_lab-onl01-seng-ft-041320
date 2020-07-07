2eb28b3c08b9758b42ec73e440f3cfd1b1530ed2
Megan Horsfield <meganthadev@gmail.com>
2020-07-07 04:14:03 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to homepage_path(@user)
     else
-      redirect_to '/new'
+      redirect_to ''
     end
   end 
   
