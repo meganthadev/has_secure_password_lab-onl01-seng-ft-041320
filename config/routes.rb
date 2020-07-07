88f7b25d4704f2a427f8d5a2c43423a49317da23
Megan Horsfield <meganthadev@gmail.com>
2020-07-07 04:10:54 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/:user', to: 'users#show', as: 'homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
